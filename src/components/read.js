516174607c7cd74c7c3950bf1edc8dd73cd25699
HaoYuYong <g00472294@atu.ie>
2024-11-22 09:39:54 +0000

    Add comment

--- src/components/read.js
+++ src/components/read.js
@@ -1,23 +1,27 @@
 import Movies from "./movies";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react"; 
+import axios from "axios"; 
 
+// The 'Read' functional component
 const Read = () => {
 
-  const [movies, setMovies] = useState([]);
+  // Declare a state variable to hold the list of movies
+  const [movies, setMovies] = useState([]); 
 
+  // useEffect hook to run a side-effect after the component mounts
   useEffect(() => {
-    
-    axios.get('http://localhost:4000/api/movies')
-      .then((response) => {
-        console.log(response.data);
-        setMovies(response.data);
+    // Make a GET request to the API endpoint to fetch the list of movies
+    axios.get('http://localhost:4000/api/movies') 
+      .then((response) => { 
+        console.log(response.data); 
+        setMovies(response.data); 
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((error) => { 
+        console.log(error); 
       });
   });
 
+  // Return the JSX to render the component
   return (
     <div>
       <h3>Hello from read component!</h3>
